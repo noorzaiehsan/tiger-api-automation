90c8dd24ba4485c57432f2ea6697dcaffaed0d5f
Ehsan Noorzai <noorzaiehsan@gmail.com>
2022-07-25 20:35:16 -0400

    My Commit Message

---
+++ src/test/java/tiger/api/testing/review/runner/TestRunner.java
@@ -0,0 +1,11 @@
+package tiger.api.testing.review.runner;
+
+import com.intuit.karate.junit5.Karate;
+
+public class TestRunner {
+
+	@Karate.Test
+	public Karate runTest() {
+		return Karate.run("classpath:features").tags("End2End");
+	}
+}
